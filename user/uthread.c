73d242a9d07eb1996588735f4895f4ffa016c9e7
omer-afori <omerafori@mail.tau.ac.il>
2024-06-05 14:38:36 +0300

    uthread WIP

--- user/uthread.c
+++ user/uthread.c
@@ -10,10 +10,27 @@
 #define STACK_SIZE  8192
 #define MAX_THREAD  4
 
+struct saved_registers {
+  uint64 ra;
+  uint64 sp;
+  uint64 s0;
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
+};
 
 struct thread {
   char       stack[STACK_SIZE]; /* the thread's stack */
   int        state;             /* FREE, RUNNING, RUNNABLE */
+  struct saved_registers regs; /* the thread's saved registers */
 };
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
